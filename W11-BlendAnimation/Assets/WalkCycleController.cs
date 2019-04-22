7b93f7b9a229ec55af4c334550a592bd2f63ac4c
Shirley Huang <shirley.huang@nyu.edu>
2019-04-22 00:35:06 -0400

    W11

---
+++ W11-BlendAnimation/Assets/WalkCycleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkCycleController : MonoBehaviour {
+    private Animator anim;
+    private float timer;
+    [Range(0,15f)]
+    public float speed;
+   
+    // Start is called before the first frame update
+    void Start(){
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timer += Time.deltaTime*speed;
+        float animSpeed = speed/15*2-1;
+        anim.SetFloat("Time", (Mathf.Sin(timer)));
+        anim.SetFloat("Speed", (Mathf.Sin(animSpeed)));
+
+    }
+}
